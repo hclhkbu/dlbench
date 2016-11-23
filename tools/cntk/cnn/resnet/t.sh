fe11637cc13d85c2270c8e24594e5e06e86dc80d
FreemanX <gzgyxpf@gmail.com>
2016-11-23 17:33:54 +0800

    testing on caffe passed, testing on cntk failed on rnn

--- tools/cntk/cnn/resnet/t.sh
+++ tools/cntk/cnn/resnet/t.sh
@@ -4,7 +4,7 @@ network=resnet
 model_file=${network}.cntk
 cp $network.cntk_template ${model_file}
 sed -i -e "s|HOME|${HOME}|g" ${model_file}
-cntk configFile=resnet.cntk deviceId=$deviceId minibatchSize=$minibatchSize maxEpochs=$epochs
+cntk configFile=resnet.cntk deviceId=$deviceId minibatchSize=$minibatchSize maxEpochs=$maxEpochs
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
 echo "GPUCount: 1" 
