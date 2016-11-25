200cb6a8e686a4e9889116441bf88df0714f95e4
shyhuai <shaohuaishi@gmail.com>
2016-11-25 11:12:06 +0800

    revise batch script of tf

--- tools/tensorflow/fc/t.sh
+++ tools/tensorflow/fc/t.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 source ~/tf11/bin/activate
 start=`date +%s.%N`
+mkdir multigpu-trained
 CUDA_VISIBLE_DEVICES=$deviceId python fcn5_mnist.py  --batch_size=$batch_size --epochs=$epochs --device_id=$deviceId
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
 echo "finished with execute time: ${runtime}" 
+rm -rf multigpu-trained
