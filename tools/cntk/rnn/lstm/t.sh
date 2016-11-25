c543eb7065acfc964878795ffb4aaebdb3dd706a
shyhuai <shaohuaishi@gmail.com>
2016-11-25 11:04:24 +0800

    revise lstm

--- tools/cntk/rnn/lstm/t.sh
+++ tools/cntk/rnn/lstm/t.sh
@@ -1,5 +1,9 @@
 start=`date +%s.%N`
-cntk configFile=rnn.cntk deviceId=$deviceId minibatch=$minibatchSize maxEpochs=$maxEpochs
+network=lstm
+model_file=${network}.cntk
+cp $network.cntk_template ${model_file}
+sed -i -e "s|HOME|${HOME}|g" ${model_file}
+cntk configFile=${network}.cntk deviceId=$deviceId minibatch=$minibatchSize maxEpochs=$maxEpochs
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
 echo "GPUCount: 1" 
