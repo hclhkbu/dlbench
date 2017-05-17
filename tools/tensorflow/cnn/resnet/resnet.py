Fixed reuse vars resuing too much and added back averaging of losses

tools/tensorflow/cnn/resnet/resnet.py
@@ -87,11 +87,9 @@ def loss(logits, labels):
     cross_entropy_mean = tf.reduce_mean(cross_entropy)
  
     regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-
-    loss_ = tf.add_n([cross_entropy_mean] + regularization_losses)
-    tf.summary.scalar('loss', loss_)
-
-    return loss_
+    total_loss = tf.add_n([cross_entropy_mean] + regularization_losses, name='total_loss')
+    
+    return total_loss
 
 
 def stack(x, c):

tools/tensorflow/cnn/resnet/resnet_cifar10_multi_gpu.py
@@ -93,16 +93,17 @@ def train():
         optimizer = tf.train.MomentumOptimizer(lr, 0.9)
 
         def assign_to_device(device, ps_device="/cpu:0"):
+            #if FLAGS.num_gpus == 1:
+            #    ps_device="/gpu:0"
             def _assign(op):
                 node_def = op if isinstance(op, tf.NodeDef) else op.node_def
-                if node_def.op == "Variable":
+                if node_def.op in ["Variable","VariableV2"]:
                     return ps_device
                 else:
                     return device
             return _assign
 
         tower_grads = []
-        average_loss_tensor = []
         reuse_variables = None
         losses = []
         for i in six.moves.range(FLAGS.num_gpus):
@@ -115,36 +116,20 @@ def train():
                     #logits = inference(images, is_training=True)
                     with tf.variable_scope(tf.get_variable_scope(), reuse=reuse_variables):
                         logits = inference_small(images, is_training=True, num_blocks=9)
-                        loss_tensor = loss(logits, labels)
-                        losses.append(loss_tensor)
-                    #tf.add_to_collection('losses', loss_tensor)
-                    #tf.add_n(tf.get_collection('losses'), name='total_loss')
-
-                    #losses = tf.get_collection('losses', n_scope)
-                    #total_loss = tf.add_n(losses, name='total_loss')
-                    # average_loss_tensor.append(loss_tensor)
-                    
-                    #update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-                    #with tf.control_dependencies(update_ops):
-                    grads = optimizer.compute_gradients(loss_tensor)
+                    tower_loss = loss(logits, labels)
+                    losses.append(tower_loss)
+                    grads = optimizer.compute_gradients(tower_loss)
                     tower_grads.append(grads)
                     reuse_variables = True
         
-        total_loss = tf.add_n(losses, name='total_loss')
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'TOWER_0')
         
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'TOWER_0')
         with tf.control_dependencies(update_ops):
+            # Average losses accross towers (GPUs)
+            total_loss = tf.reduce_mean(losses, 0)
             grads = average_gradients(tower_grads)
             apply_gradient_op = optimizer.apply_gradients(grads, global_step=global_step)
         train_op = apply_gradient_op
-
-        #update_ops.append(apply_gradient_op)
-
-        #update_op = tf.group(*update_ops)
-        #train_op = control_flow_ops.with_dependencies([update_op], total_loss,
-        #                                              name='train_op')
-
-        # average_op = tf.reduce_mean(average_loss_tensor, 0)
 
         # Create a saver.
         saver = tf.train.Saver(tf.global_variables())