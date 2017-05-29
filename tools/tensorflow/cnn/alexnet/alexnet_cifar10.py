3de96add843fc3157b1229a2d8a90fafacd01f71
Toby Boyd <tobyboyd@google.com>
2017-05-28 19:34:35 -0700

    Changed to NCHW and added DataSet support

--- tools/tensorflow/cnn/alexnet/alexnet_cifar10.py
+++ tools/tensorflow/cnn/alexnet/alexnet_cifar10.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 import time
 import cifar10_input
-#import unpickle as cifar10_input
 
 import tensorflow as tf
 import numpy as np
@@ -34,6 +33,7 @@ tf.app.flags.DEFINE_boolean('use_fp16', False,
                             """Train the model using fp16.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
+tf.app.flags.DEFINE_boolean('use_dataset', False, """True to use datasets""")
 
 data_format = 'NCHW'
 data_format_c = 'channels_first'
@@ -52,20 +52,9 @@ def get_device_str(device_id):
 
 def _conv(inpOp, nIn, nOut, kH, kW, dH, dW, padType):
     global conv_counter
-    global parameters
     name = 'conv' + str(conv_counter)
     conv_counter += 1
-    print('conv_name:{}'.format(name))
     with tf.variable_scope(name):
-        #kernel = tf.Variable(tf.truncated_normal([kH, kW, nIn, nOut],
-                                                # dtype=tf.float32,
-                                                # stddev=1e-2), name='weights')
-        #kernel = tf.Variable(tf.random_normal([kH, kW, nIn, nOut],
-        #                                         dtype=tf.float32,
-        #                                         stddev=1e-2), name='weights')
-        #strides = [1, 1, dH, dW]
-        #conv = tf.nn.conv2d(inpOp, kernel, strides, padding=padType, data_format=data_format)
-
         kernel_initializer = tf.truncated_normal_initializer(stddev=1e-2)
         conv = tf.layers.conv2d(inpOp,
                         nOut, 
@@ -75,17 +64,12 @@ def _conv(inpOp, nIn, nOut, kH, kW, dH, dW, padType):
                         data_format=data_format_c,
                         kernel_initializer=kernel_initializer,
                         use_bias=False)
-
-        print('conv before biases:',conv)
-
-        #biases = tf.Variable(tf.constant(0.0, shape=[nOut], dtype=tf.float32),
-                             #trainable=True, name='biases')
         biases = tf.get_variable(
                         'biases', [nOut], tf.float32,
                         tf.constant_initializer(0.0))
+
         bias = tf.reshape(tf.nn.bias_add(conv, biases, data_format=data_format),
                           conv.get_shape())
-        #parameters += [kernel, biases]
         return bias
 
 
@@ -95,10 +79,7 @@ def _relu(inpOp):
 
 def _padding(inpOp, pad):
     padded_input = tf.pad(inpOp, [[0, 0], [0, 0], [pad, pad], [pad, pad]], "CONSTANT")
-    #padded_input = tf.pad(inpOp, [[0, 0], [pad, pad], [pad, pad], [0, 0]], "CONSTANT")
-    print('padded_input: ', padded_input)
     return padded_input
-    #return inpOp
 
 
 def _norm(inpOp, local_size, alpha, beta):
@@ -114,19 +95,6 @@ def _affine(inpOp, nIn, nOut):
     name = 'affine' + str(affine_counter)
     affine_counter += 1
     with tf.name_scope(name) as scope:
-        #kernel = tf.Variable(tf.truncated_normal([nIn, nOut],
-        #                                         dtype=tf.float32,
-        #                                         stddev=1e-2), name='weights')
-        
-        #kernel = tf.Variable(tf.random_normal([nIn, nOut],
-        #                                         dtype=tf.float32,
-        #                                         stddev=1e-2), name='weights')
-        
-        #biases = tf.Variable(tf.constant(0.0, shape=[nOut], dtype=tf.float32),
-        #                     trainable=True, name='biases')
-        #affine1 = tf.nn.relu_layer(inpOp, kernel, biases, name=name)
-        #parameters += [kernel, biases]
-        #return affine1
         kernel = tf.get_variable(
             'weights', [nIn, nOut],
             tf.float32,
@@ -142,19 +110,11 @@ def _mpool(inpOp, kH, kW, dH, dW):
     global parameters
     name = 'pool' + str(pool_counter)
     pool_counter += 1
-    #ksize = [1, kH, kW, 1]
-    #strides = [1, 1, dH, dW ]
     return tf.layers.max_pooling2d(
         inpOp, [kH, kW], [dH, dW],
         padding='VALID',
         data_format=data_format_c,
         name=name)    
-    #return tf.nn.max_pool(inpOp,
-    #                      ksize=ksize,
-    #                      strides=strides,
-    #                      padding='VALID',
-    #                      name=name,
-    #                      data_format=data_format)
 
 def _avgpool(inpOp, kH, kW, dH, dW):
     global pool_counter
@@ -167,39 +127,19 @@ def _avgpool(inpOp, kH, kW, dH, dW):
         padding='VALID',
         data_format=data_format_c,
         name=name)
-    #return tf.nn.avg_pool(inpOp,
-    #                      ksize=ksize,
-    #                      strides=strides,
-    #                      padding='VALID',
-    #                      name=name,
-    #                      data_format=data_format)
 
 def loss(logits, labels):
-    print(labels)
-    batch_size = tf.size(labels)
-    labels = tf.expand_dims(labels, 1)
-    indices = tf.expand_dims(tf.range(0, batch_size, 1), 1)
-    concated = tf.concat(axis=1, values=[indices, labels])
-    onehot_labels = tf.sparse_to_dense(
-        concated, tf.stack([batch_size, 10]), 1.0, 0.0)
-    print(logits)
-    print(onehot_labels)
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits,
-                                                            labels=onehot_labels,
+                                                            labels=labels,
                                                             name='xentropy')
     loss = tf.reduce_mean(cross_entropy, name='xentropy_mean')
     return loss
 
 def inference(images):
-    print('images Interence########{}'.format(images))
-    images = tf.transpose(images, [0, 3, 1, 2])
     pad1 = _padding(images, 2) 
     conv1 = _conv (pad1, 3, 32, 5, 5, 1, 1, 'VALID')
-    print('conv1:{}'.format(conv1))
     pool1 = _mpool(conv1,  3, 3, 2, 2)
-    print('pool1:{}'.format(conv1))
     relu1 = _relu(pool1)
-    print('REUL:{}'.format(relu1))
     #norm1 = _norm(relu1, 3, 5e-05, 0.75)
 
     pad2 = _padding(relu1, 2)
@@ -213,43 +153,7 @@ def inference(images):
     relu3 = _relu(conv3)
     pool3 = _avgpool(relu3, 3, 3, 2, 2) 
 
-    #resh1 = tf.reshape(pool3, [-1, 64 * 3 * 3])
-    print('pool3: {}'.format(pool3))
-
     resh1 = tf.reshape(pool3, [-1, 64 * 3 * 3])
-    print('post reshape:{} '.format(resh1))
-
-    affn1 = _affine(resh1, 64*3*3, 10)
-
-    shape = affn1.get_shape().as_list()
-    flat_dim = shape[1]
-    #resh1 = tf.reshape(pool3, [-1,flat_dim])
-
-    return affn1
-
-def inference2(images):
-    images = tf.transpose(images, [0, 3, 1, 2])
-    #pad1 = _padding(images, 2) 
-    conv1 = _conv (images, 3, 32, 5, 5, 1, 1, 'SAME')
-    pool1 = _mpool(conv1,  3, 3, 2, 2)
-    relu1 = _relu(pool1)
-    #norm1 = _norm(relu1, 3, 5e-05, 0.75)
-
-    #pad2 = _padding(relu1, 2)
-    conv2 = _conv (relu1,  32, 32, 5, 5, 1, 1, 'SAME')
-    pool2 = _mpool(conv2,  3, 3, 2, 2)
-    relu2 = _relu(pool2)
-    #norm2 = _norm(relu2, 3, 5e-05, 0.75)
-
-    #pad3 = _padding(relu2, 2)
-    conv3 = _conv (relu2,  32, 64, 5, 5, 1, 1, 'SAME')
-    relu3 = _relu(conv3)
-    pool3 = _avgpool(relu3, 3, 3, 2, 2) 
-    print('pool3: {}'.format(pool3))
-
-    resh1 = tf.reshape(pool3, [-1, 64 * 3 * 3])
-    print('post reshape:{} '.format(resh1))
-
     affn1 = _affine(resh1, 64*3*3, 10)
 
     return affn1
@@ -258,28 +162,30 @@ def inference2(images):
 def train():
   global parameters
   config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=FLAGS.log_device_placement)
-  #config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
+  config.intra_op_parallelism_threads = 1
+  config.inter_op_parallelism_threads = 0
   device_str = get_device_str(FLAGS.device_id)
   if device_str.find('cpu') >= 0: # cpu version
       num_threads = os.getenv('OMP_NUM_THREADS', 1)
       print 'num_threads: ', num_threads
       config = tf.ConfigProto(allow_soft_placement=True, intra_op_parallelism_threads=int(num_threads))
   with tf.Graph().as_default(), tf.device(device_str), tf.Session(config=config) as sess:
-      #image_size = 32 
-      #images, labels = cifar10_input.distorted_inputs(FLAGS.data_dir, FLAGS.batch_size)
-      #images, labels = cifar10_input.inputs(False, FLAGS.data_dir, FLAGS.batch_size)
+      initalizer = None
+      images = None
+      labels = None
       with tf.device('/cpu:0'):
-        images, labels = cifar10_input.inputs(False, FLAGS.data_dir, FLAGS.batch_size)
-        print(images)
-      print('Images: ', images)
+        if FLAGS.use_dataset:
+          iterator, initalizer =  cifar10_input.dataSet(FLAGS.data_dir, FLAGS.batch_size)
+          images, labels = iterator.get_next()
+        else:
+          images, labels = cifar10_input.inputs(False, FLAGS.data_dir, FLAGS.batch_size)
 
+      labels = tf.contrib.layers.one_hot_encoding(labels, 10)
       logits = inference(images)
-      #logits = inference2(images)
       # Add a simple objective so we can calculate the backward pass.
       loss_value = loss(logits, labels)
       # Compute the gradient with respect to all the parameters.
       lr = 0.001
-      #grad = tf.train.GradientDescentOptimizer(lr).minimize(loss_value)
       grad = tf.train.MomentumOptimizer(lr, 0.9).minimize(loss_value)
 
       # Create a saver.
@@ -289,9 +195,13 @@ def train():
       init = tf.global_variables_initializer()
       # Start running operations on the Graph.
       sess.run(init)
-      coord = tf.train.Coordinator()
-      threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-
+      coord = None
+      threads = None
+      if FLAGS.use_dataset:
+        sess.run(initalizer)
+      else:
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
       real_batch_size = FLAGS.batch_size
       num_batches_per_epoch = int((EPOCH_SIZE + real_batch_size - 1)/ real_batch_size)
       iterations = FLAGS.epochs * num_batches_per_epoch 
@@ -319,8 +229,9 @@ def train():
           if step == iterations-1:
               checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
               saver.save(sess, checkpoint_path, global_step=step)
-      coord.request_stop()
-      coord.join(threads)
+      if not FLAGS.use_dataset:
+        coord.request_stop()
+        coord.join(threads)
       average_batch_time /= iterations
       print 'average_batch_time: ', average_batch_time
       print ('epoch_info: %s' % ','.join(epochs_info))
