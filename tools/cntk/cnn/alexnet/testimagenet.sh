dea21c477e313efb2ef34224711f6e12cc673139
shyhuai <shaohuaishi@gmail.com>
2017-07-29 01:27:36 +0800

    local change

--- tools/cntk/cnn/alexnet/testimagenet.sh
+++ tools/cntk/cnn/alexnet/testimagenet.sh
@@ -1,2 +1,2 @@
-mpirun -np 16 -machinefile cluster16 cntk configFile=alexnet_imagenet2.cntk deviceId=auto minibatchSize=256 maxEpochs=40 parallelTrain=true command=Train
+#mpirun -np 16 -machinefile cluster16 cntk configFile=alexnet_imagenet2.cntk deviceId=auto minibatchSize=256 maxEpochs=40 parallelTrain=true command=Train
 #cntk configFile=alexnet_imagenet2.cntk deviceId=auto minibatchSize=256 command=Test
