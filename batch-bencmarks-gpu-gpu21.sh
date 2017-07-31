587ee8e3b76fe00fe1a176396bc828bc6d913850
shyhuai <shaohuaishi@gmail.com>
2017-07-31 16:59:13 +0800

    add multiple nodes support

---
+++ batch-bencmarks-gpu-gpu21.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# The benchmarks of all toolkits 
+#python benchmark.py -config ./configs/gpuk80.config -post True
+python benchmark.py -config ./configs/tensorflowbm6_gpu21.config -post True
